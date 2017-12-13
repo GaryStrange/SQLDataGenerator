5e05ff89e0068739f379e579b071eb3859324aea
Gary Strange <gary.strange@asos.com>
2017-12-13 15:02:09 +0000

    Add drop view sproc

---
+++ SQLDataGenerator/SQLDataGenerator/Procedures/DropView.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[DropView]
+	@view_name SYSNAME,
+	@schema_name SYSNAME = 'dbo'
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @sql_statement NVARCHAR(4000) = N'DROP VIEW {2}{0}.{1}';
+
+	SET @sql_statement = REPLACE(@sql_statement, '{0}', @schema_name)
+	SET @sql_statement = REPLACE(@sql_statement, '{1}', @view_name)
+
+	--TODO: Check server edition
+	SET @sql_statement = REPLACE(@sql_statement, '{2}', 'IF EXISTS ')
+
+	PRINT @sql_statement
+	EXEC( @sql_statement )
+END
