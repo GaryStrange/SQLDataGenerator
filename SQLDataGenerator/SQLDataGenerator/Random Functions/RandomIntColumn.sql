eb6043c4bcebb39ac2e8cf9645825974d9cbcbc1
Gary Strange <gary.strange@asos.com>
2018-06-12 15:54:44 +0100

    Add RandomIntColumn

---
+++ SQLDataGenerator/SQLDataGenerator/Random Functions/RandomIntColumn.sql
@@ -0,0 +1,10 @@
+﻿CREATE FUNCTION [dbo].[RandomIntColumn]
+(
+	@Guid uniqueidentifier
+)
+RETURNS TABLE AS RETURN
+(
+	SELECT rnc.*
+	FROM dbo.IntConst() Int32
+	CROSS APPLY  [dbo].[RandomNumberColumn](1, Int32.MaxInt, @Guid) rnc
+)
